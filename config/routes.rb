69d445b1305daadb598d4f500648fe55109250dd
taji <taji@sonicgarden.jp>
2026-01-09 16:07:25 +0900

    色々

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
   post '/graphql', to: 'graphql#execute'
-  root 'daily_reports#index'
+  root 'daily_reports#new'
   resources :daily_reports, only: %i[index show new edit create update destroy]
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
